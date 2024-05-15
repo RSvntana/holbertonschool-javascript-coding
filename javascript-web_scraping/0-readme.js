585e68428a05602ffcc3281ab603bf070443bdd2
Robert <123roabel@gmail.com>
2024-05-15 10:26:55 -0400

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -3,18 +3,12 @@
 
 const fs = require('fs');
 
-function readFile(filePath) {
-  try {
-    const content = fs.readFileSync(filePath, 'utf8');
-    console.log(content);
-  } catch (err) {
-    console.error(err);
-  }
-}
+const filePath = process.argv[2];
 
-if (process.argv.length > 2) {
-  const filePath = process.argv[2];
-  readFile(filePath);
-} else {
-  console.error('Please provide a file path as an argument.');
-}
+fs.readFile(filePath, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
